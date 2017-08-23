c2ad1e8a2ad0d8e3b02de0a9db585ccd1bc558d5
AlanP <pwalan@foxmail.com>
2017-08-23 15:50:09 +0800

    新的包结构

--- src/main/java/com/github/pwalan/MyApplication.java
+++ src/main/java/com/github/pwalan/MyApplication.java
@@ -1,5 +1,6 @@
 package com.github.pwalan;
 
+import com.github.pwalan.config.LearnerSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/github/pwalan/config/LearnerSettings.java
+++ src/main/java/com/github/pwalan/config/LearnerSettings.java
@@ -1,4 +1,4 @@
-package com.github.pwalan;
+package com.github.pwalan.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
